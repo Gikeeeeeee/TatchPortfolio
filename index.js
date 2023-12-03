f51f6463c79de398277812d7d3e92166d3bfa74d
Wishkyx <106916482+Gikeeeeeee@users.noreply.github.com>
2023-12-03 19:03:50 +0700

    Update index.js
    More Function
    - Scroll down animation
    - new autotyping text

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-// autotypingHomepage 
+/*=============== autotypingHomepage ===============*/
 var typed = new Typed('.autotyped', {
-  strings: ['Computer Science.', 'Computer.', 'Coding.','Programming.'],
+  strings: ['Digital Technology.','Software.', 'Computer.', 'Coding.', 'Programming.'],
   typeSpeed: 200,
   backSpeed: 150,
   loop: true
 });
 
+/*=============== navbars-crolldown ===============*/
 let nav = document.querySelector("nav");
-    window.onscroll = function() {
-      if(document.documentElement.scrollTop > 20){
-        nav.classList.add("sticky");
-      }else {
-        nav.classList.remove("sticky");
-      }
-    }
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+/*=============== scroll down animation ===============*/
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 1500,
+})
+
+sr.reveal(`.home-info, .welcome-info, .certificate-item`,{delay: 100 ,origin: 'left'})
+sr.reveal(`.title, .profile, .timeline-grade, .education-info, .category, .footer-content`,{delay: 300 ,origin: 'right'})
+sr.reveal(`.school-logo`,{delay: 600 ,origin: 'right'})
+sr.reveal(`.about-info, .myinformation, .project-content`,{delay: 300 ,origin: 'bottom'})
